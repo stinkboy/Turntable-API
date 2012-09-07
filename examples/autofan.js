b47adb2d56e94d24f4b41075b9f5f9dbad144d1c
stinkboy <nickbarstatis@yahoo.com>
2012-09-07 16:41:55 -0500

    Update examples/autofan.js

--- examples/autofan.js
+++ examples/autofan.js
@@ -3,9 +3,9 @@
  */
 
 var Bot    = require('../index');
-var AUTH   = 'auth+live+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
-var USERID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
-var ROOMID = 'xxxxxxxxxxxxxxxxxxxxxxxx';
+var AUTH   = auth+live+739daf39160c0468730f30999c39d75a090beeac
+var USERID = 50169ea9aaa5cd2e8c000277
+var ROOMID = 501847a8df5bcf31c880d333
 
 var bot = new Bot(AUTH, USERID, ROOMID);
 
